6d1b74fead7792fe21d092fca709f93fcf601da3
Kristi Rajandu <94612028+Cristallic89@users.noreply.github.com>
2022-11-10 13:36:53 +0200

    API nr2
    Kristi Rajandu & Kädi Märss

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
-    console.log(`Node Todo API is running on port: ${port}`)
+    console.log(`Node user API is running on port: ${port}`)
 })
 
-const todos = [
+const users = [
     { id: 1, email: "nethog@hot.ee", username:"hogwards", password:"AssaVana112!@"},
     { id: 2, email: "rusty@hot.ee", username:"rustycar", password:"AssaVana911!@"},
     { id: 3, email: "laulumiis@hot.ee", username:"laulumiis", password:"AssaVana007!@"},
@@ -21,76 +21,76 @@ const todos = [
 ]
 
 
-app.get('/todos', function (req, res) {
-    return res.send(todos)
+app.get('/users', function (req, res) {
+    return res.send(users)
 });
 
-app.get('/todos/:id', function (req, res) {
+app.get('/users/:id', function (req, res) {
     const id = req.params.id;
     let result = null
-    for (let i = 0; i < todos.length; i++) {
-        const todo = todos[i];
-        if (todo.id == id) { // using == instead of === because id is a string.
-            result = todo;
+    for (let i = 0; i < users.length; i++) {
+        const user = users[i];
+        if (user.id == id) { // using == instead of === because id is a string.
+            result = user;
         }
     }
     return res.send(result);
 });
 
-app.post('/todos/', function (req, res) {
-    const newId = todos.length + 1;
-    const newTodo = {
+app.post('/users/', function (req, res) {
+    const newId = users.length + 1;
+    const newuser = {
         id: newId,
-        todo: req.body.todo,
+        user: req.body.user,
         completed: false
     }
-    todos.push(newTodo)
+    users.push(newuser)
 
-    return res.send(todos);
+    return res.send(users);
 });
 
-app.put('/todos/', function (req, res) {
+app.put('/users/', function (req, res) {
 
-    //  Find the todo to update by ID
+    //  Find the user to update by ID
 
-    let todoToUpdate = todos.find((todo) => {
-        return todo.id == req.body.id
+    let userToUpdate = users.find((user) => {
+        return user.id == req.body.id
     })
 
-    todoToUpdate = {
+    userToUpdate = {
         id: req.body.id,
-        todo: req.body.todo,
+        user: req.body.user,
         completed: req.body.completed
     };
 
 
-    //  Find the index of that todo to update.
+    //  Find the index of that user to update.
 
-    let index = todos.findIndex((todo) => {
-        return todo.id == req.body.id
+    let index = users.findIndex((user) => {
+        return user.id == req.body.id
     });
 
 
-    // Update the todo in the list
+    // Update the user in the list
 
-    todos[index] = todoToUpdate;
+    users[index] = userToUpdate;
 
 
     //  Return the response
 
-    return res.send(todos);
+    return res.send(users);
 });
 
-app.delete('/todos/:id', function (req, res) {
+app.delete('/users/:id', function (req, res) {
 
-    //  Find the index of that todo to update.
-    let index = todos.findIndex((todo) => {
-        return todo.id == req.params.id
+    //  Find the index of that user to update.
+    let index = users.findIndex((user) => {
+        return user.id == req.params.id
     });
 
 
-    todos.splice(index, 1);
+    users.splice(index, 1);
 
     //  Return the response
-    return res.send(todos);
+    return res.send(users);
 });
